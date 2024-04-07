74465d79bf6a81863cf0235e98990923d0285cdb
tripsycodes <145247047+tripsycodes@users.noreply.github.com>
2024-04-07 19:39:08 +0530

    Update mlearning.h

--- mlearning.h
+++ mlearning.h
@@ -24,34 +24,40 @@ int shape(void *arr, int dim_size)
     return shape;
 }
 
+//To return an equally spaced array
 double* linspace(double start, double end, int n) {
      double *x;
      int i = 0;
-     double step = (end - start) / ((double)n-1);
-     x = (double*)calloc(n, sizeof(double));
+     double step = (end - start) / ((double)n-1);    //the space between to consecutive elements
+     x = (double*)calloc(n, sizeof(double));    //allocation of memory and initialisation woth 0
      x[i] = start; 
 
-     for (i = 1; i < n; i++) {
+    i=1;
+     while(i<n){
          x[i] = x[i - 1] + step;
+         i++;
      }
      x[n - 1] = end;
      return x;
      }
 
-
+//to return an array equally spaced on the logarithmic scale
 double* logspace(double start, double end, int n, double base) {
-    double* result = (double*)calloc(n,sizeof(double));
+    double* result;
+    result = (double*)calloc(n,sizeof(double));
     double log_start = log10(start) / log10(base);
     double log_end = log10(end) / log10(base);
-    double step = (log_end - log_start) / (n - 1);
+    double step = (log_end - log_start) / (n - 1);    //calculating the difference between 2 numbers ont he logarithmic scale
 
-    for (int i = 0; i < n; i++) {
+    int i = 0;
+    while(i<n){
         result[i] = pow(base, log_start + i * step);
+        i++;
     }
     return result;
 }
 
-
+// Values are generated within the half-open interval [start, stop), with spacing between values given by step.
 double* arange(double start, double end, double step) {
     int n = (int)((end - start) / step) + 1;
     double* result = (double*)calloc(n , sizeof(double));
