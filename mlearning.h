0f9fbfe52c426ff714c359ae2fe0b68fd580f13d
tripsycodes <145247047+tripsycodes@users.noreply.github.com>
2024-04-10 19:21:12 +0530

    Update mlearning.h

--- mlearning.h
+++ mlearning.h
@@ -8,7 +8,9 @@
         exit(1);                                        \
     }                                                   \
 } while (0)
+
 #define PI 3.14159265
+#define float double
 
 int size(int*arr){
     int out = sizeof(arr)/sizeof(arr[0]);
@@ -499,6 +501,62 @@ void *reshape_1d_3d(size_t id1, int* iar,
     return oar;
 }
 
+void regression(int n,int ax[],int ay[])
+{
+    int i;
+    float x, y, m, c, d;
+    float sumx = 0, sumxsq = 0, sumy = 0, sumxy = 0;
+    float sumysq = 0;
+    float mae = 0;
+    float mse = 0;
+    float p = n;
+    for(int i=0;i<n;i++)
+    {
+        sumx = sumx + x;
+        sumxsq = sumxsq + (x * x);
+        sumy = sumy + y;
+        sumxy = sumxy + (x * y);
+        sumysq = sumysq + (y * y);
+        ax[i] = x;
+        ay[i] = y;
+    }
+    d = p * sumxsq - sumx * sumx;
+    m = (p* sumxy - sumx * sumy) / d;
+    c = (sumy * sumxsq - sumx * sumxy) / d;
+  while(1)
+  {
+    printf("1.compute sum\n");
+    printf("2.find best fitting line\n");
+    printf("3.end\n");
+    int num;
+    scanf("%d", &num);
+    if (num == 1)
+    {
+        printf("sum x = %lf\n", sumx);
+        printf("sum y = %lf\n", sumy);
+        printf("sum xy = %lf\n", sumxy);
+        printf("sum xsq = %lf\n", sumxsq);
+        printf("sum ysq = %lf\n", sumysq);
+    }
+    else if (num == 2)
+    {
+        float p = n;
+        printf("the best fitting line is y = %lfx+%lf\n", m, c);
+        for (int i = 0; i < n; i++)
+        {
+            mae += abs(ay[i] - (m * ax[i] + c));
+            mse += (ay[i] - (m * ax[i] + c)) * (ay[i] - (m * ax[i] + c));
+        }
+        printf("mean absolute error = %lf\n", mae / p);
+        printf("mean squared error = %lf\n", mse / p);
+    }
+    else if(num==3)
+    {
+        return 0;
+    }
+  }
+
+}
 
 
 
